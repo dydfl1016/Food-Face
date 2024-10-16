6aebcc7a8826b73916e4a96c3b3e4b50f173be55
dydfl1016 <dydfl1016@gmail.com>
2024-10-17 06:04:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -23,9 +23,16 @@ function drop(e) {
     const src = e.dataTransfer.getData('text/plain');
     const img = document.createElement('img');
     img.src = src;
+    
+    // 크기를 고정함 (50x50 픽셀로 설정)
+    img.style.width = '50px';
+    img.style.height = '50px';
+    
+    // 이미지 위치 설정
     img.style.position = 'absolute';
-    img.style.left = `${e.clientX - faceArea.offsetLeft - 25}px`; // 이미지 중앙 정렬을 위한 오프셋 조정
-    img.style.top = `${e.clientY - faceArea.offsetTop - 25}px`;
+    img.style.left = `${e.clientX - faceArea.offsetLeft - 25}px`; // 중앙 정렬
+    img.style.top = `${e.clientY - faceArea.offsetTop - 25}px`;   // 중앙 정렬
     img.classList.add('food-on-face');
+    
     faceArea.appendChild(img);
 }
